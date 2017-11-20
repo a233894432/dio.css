038059d04038738339a8da701d7069f768b29874
a233894432 <a233894432@gmail.com>
2017-11-20 16:38:02 +0800

    增加城市选择树插件

---
+++ example/cityTree/citySelect.js
@@ -0,0 +1,97 @@
+/**
+ * 城市选择插件
+ * 
+ * Created by diogo on 2017年11月20日15:54:03
+ */
+"use strict";
+define(["jquery"], function($) {
+  var treedata, cityArray, cityO;
+
+  //   创建树
+  function creatTree(data, parent) {
+    var array = [],
+      temp;
+    for (var i = 0, l = data.length; i < l; i++) {
+      if (data[i].pId == parent) {
+        data[i] = {
+          id: data[i].id.toString(),
+          text: data[i].name.toString(),
+          value: data[i].id.toString() + "|" + data[i].pId.toString(),
+          showcheck: true,
+          complete: true,
+          isexpand: false,
+          checkstate: 0,
+          hasChildren: true,
+          ChildNodes: new Array()
+        };
+
+        array.push(data[i]);
+        temp = creatTree(data, data[i].id);
+        if (temp.length > 0) {
+          data[i].ChildNodes = temp;
+        } else {
+          data[i].hasChildren = false;
+        }
+      }
+    }
+    return array;
+  }
+
+  //递归选中
+  function doCheck(items, c, fn) {
+    for (var i = 0, l = items.length; i < l; i++) {
+      if (items[i].id == c.provinceId) {
+        fn(items[i], 2);
+      }
+      items[i].id == c.cityId && items[i].checkstate != 1 && fn(items[i], 1);
+
+      if (items[i].ChildNodes != null && items[i].ChildNodes.length > 0) {
+        doCheck(items[i].ChildNodes, c, fn);
+      }
+    }
+  }
+
+  var cSelect = (function() {
+    // console.log("object");
+    return {
+      // 项目初始化.
+      init: function() {
+        console.log("init");
+      },
+      /**
+       * @param {{eid}}  活动ID
+       * @param {{dom}}  DOM  id
+       * @param {{url}}  请求 地址的 URL
+       */
+      getCitys: function(eid, dom, url) {
+        var temp = "?_=" + new Date().getTime();
+        var url = url + temp;
+        $.post(
+          url,
+          null,
+          function(data) {
+            if (data.code == "200") {
+              //                  	debugger
+              cityArray = creatTree(data.data, 0);
+              cityO = {
+                showcheck: true,
+                data: cityArray
+              };
+
+              cityArray[0].isexpand = true;
+              $(dom).treeview(cityO);
+              // if (id) {
+              //   getIsCitys(id);
+              // }
+            } else {
+              alert(data.msg);
+            }
+          },
+          "json"
+        );
+      }
+    };
+  })();
+
+  return cSelect;
+});
